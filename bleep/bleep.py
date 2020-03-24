ce0b5b72429ec7019ea0213f952f15e3e9f844d7
Classroom <classroom@CS-CLASSROOM.local>
2020-03-24 13:15:28 -0400

    Add bleep and scope

--- bleep/bleep.py
+++ bleep/bleep.py
@@ -1,6 +1,16 @@
 from cs50 import get_string
 from sys import argv
 
+QUARTER = 25
+
+def foo(a, bad_words_file='banned.txt'):
+    pass
+
+def squares(nums=None):
+    if nums is None:
+        nums = [1, 2, 3]
+    return [x * x for x in nums]
+
 
 def main():
 
--- python-extras/scope.py
+++ python-extras/scope.py
@@ -1,12 +1,13 @@
 def swap(x, y):
     x, y = y, x
+    print(f'x -> {x}, y -> {y}')
 
 def test_swap():
     x, y = 2, 3
     print(f'x -> {x}, y -> {y}')
     x, y = y, x
     print(f'x -> {x}, y -> {y}')
-    swap(x, y)
+    x,y = swap(x, y)
     print(f'x -> {x}, y -> {y}')
 
 a = 99
@@ -16,6 +17,7 @@ def scope():
     print(f'a -> {a}')
 
 def test_scope():
+    global NICKLE, DIME, QUARTER
     print(f'a -> {a}')
     a = 4
     print(f'a -> {a}')
